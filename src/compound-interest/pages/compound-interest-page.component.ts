abc961699c9d78f6b32c398302bd605139b4be70
Speuce <mrk5199@gmail.com>
2021-09-25 21:02:39 -0500

    Add Sliders for all fields

--- src/compound-interest/pages/compound-interest-page.component.ts
+++ src/compound-interest/pages/compound-interest-page.component.ts
@@ -3,15 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-compound-interest-page',
   template: `
-    <app-year-selector></app-year-selector>
-    <app-linear-slider-value
-      [min]="0.0"
-      [max]="100.0"
-      [step]="0.05"
-      suffix="%"
-      title="Interest Rate"
-      subtitle="Annual"
-    ></app-linear-slider-value>
+    <div style="display: flex; flex-wrap: wrap; align-items: end">
+      <app-year-selector></app-year-selector>
+      <app-linear-slider-value
+        [min]="0.0"
+        [max]="100.0"
+        [step]="0.05"
+        [arrowStep]="1"
+        suffix="%"
+        title="Interest Rate"
+        subtitle="Annual"
+        style="display: block; margin-top: 30px;"
+      ></app-linear-slider-value>
+      <app-linear-slider-value
+        [min]="0.0"
+        [max]="1000000"
+        [step]="10000"
+        [textWidth]="150"
+        softMax
+        prefix="$"
+        title="Principle"
+        style="display: block; margin-top: 30px;"
+      ></app-linear-slider-value>
+
+      <app-linear-slider-value
+        [min]="0.0"
+        [max]="100000"
+        [step]="1000"
+        [textWidth]="150"
+        softMax
+        prefix="$"
+        title="Annual Addition"
+        style="display: block; margin-top: 30px;"
+      ></app-linear-slider-value>
+    </div>
   `,
 
   styles: [],
